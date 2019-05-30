adb3d1f63a5152e7bdbab1deee64edad908c3496
Lucas <lucas.tnagel@gmail.com>
2019-05-30 06:34:37 -0400

    Adding upvote and downvote buttons

--- WebApp/Topics/views.py
+++ WebApp/Topics/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import HttpResponse
+from django.shortcuts import HttpResponseRedirect
 from .models import TopicInformation
-from django.template import loader
+from django.urls import reverse
 from django.shortcuts import render
 from django.http import Http404
 
@@ -19,7 +19,25 @@ def detail(request, id):
         Topic = TopicInformation.objects.get(pk=id)
     except TopicInformation.DoesNotExist:
         raise Http404("Topic does not exist.")
+
     return render(request, 'Topics/detail.html', {'Topic': Topic})
+
+
+def upVote(request, id):
+    Topic = TopicInformation.objects.get(pk=id)
+    Topic.votes += 1
+    Topic.save()
+
+    return HttpResponseRedirect(reverse('Topics:detail', args=[id]))
+
+
+def downVote(request, id):
+    Topic = TopicInformation.objects.get(pk=id)
+    Topic.votes -= 1
+    Topic.save()
+
+    return HttpResponseRedirect(reverse('Topics:detail', args=[id]))
+
 
 
 # Create your views here.
