31e4ad4a1b1612cb371adde03c93977e24e92e76
Ocimar.Acad <ocimar.acad@gmail.com>
2019-05-30 17:57:07 -0300

    Update with master

--- Topics/urls.py
+++ Topics/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 
 from . import views
 
+app_name = 'Topics'
+
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:id>/', views.detail, name='detail')
+    path('<int:id>/', views.detail, name='detail'),
+    path('<int:id>/upvote', views.upVote, name='upvote'),
+    path('<int:id>/downvote', views.downVote, name='downvote')
 ]
