29a0dc7c1ebcc1a2a45cea2eca057784cf9c568a
Lucas <lucas.tnagel@gmail.com>
2019-06-22 10:04:45 -0400

    Creating topic creation tool

--- Topics/views.py
+++ Topics/views.py
@@ -3,7 +3,8 @@ from .models import TopicInformation
 from django.urls import reverse
 from django.shortcuts import render
 from django.http import Http404
-
+from django.contrib import messages
+from django.utils import timezone
 
 def index(request):
     best_topic = TopicInformation.objects.order_by('votes')
@@ -37,6 +38,24 @@ def downVote(request, id):
     Topic.save()
 
     return HttpResponseRedirect(reverse('Topics:detail', args=[id]))
+
+
+def newEntry(request):
+    if request.method == 'POST':
+        if request.POST.get('title') and request.POST.get('small_description') and request.POST.get('big_description'):
+            new_topic = TopicInformation()
+            new_topic.title_text = request.POST.get('title')
+            new_topic.small_description = request.POST.get('small_description')
+            new_topic.big_description = request.POST.get('big_description')
+            new_topic.pub_date = timezone.now()
+            new_topic.votes = 0
+            new_topic.save()
+            return render(request, 'Topics/detail.html', {'Topic': new_topic})
+        else:
+            messages.success(request, 'Fill all the spaces, before submitting.')
+            return render(request, 'Topics/new_entry.html')
+    else:
+        return render(request, 'Topics/new_entry.html')
 
 
 
